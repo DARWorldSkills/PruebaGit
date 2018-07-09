0bfb431e0486fd9ea4622b552c7b3d3ee74040c2
Rodrigo A <rodseiya20008@gmail.com>
2018-07-09 14:24:57 -0500

    first commit

---
+++ Prueba/app/src/main/java/com/software/ragp/prueba/Splash.java
@@ -0,0 +1,25 @@
+package com.software.ragp.prueba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(timerTask, 1500);
+    }
+}
